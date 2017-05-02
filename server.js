631f131792c21f98b79b9f5a5df2760cb97ab809
Ahmed Salah <jdev.cs2011@gmail.com>
2017-05-02 06:36:44 +0200

    update

--- server.js
+++ server.js
@@ -26,8 +26,9 @@ var client = mqtt.createClient(1883, "broker.mqtt-dashboard.com");
 
 
 
-//var client = mqtt.createClient(1883, "iot.eclipse.org");
+var client2 = mqtt.createClient(1883, "iot.eclipse.org");
 
+         
 
 /*
 
@@ -79,6 +80,8 @@ mongoose.connect('mongodb://admin:password@ds031721.mongolab.com:31721/egyiotpor
 			for(var i=0;i<sensors.length;i++){
 				if(sensors[i].type !=  "actuator"){
             client.subscribe(sensors[i]._id);
+			            client2.subscribe(sensors[i]._id);
+
 							console.log("Registered to "+sensors[i]._id);
 
 				}
@@ -284,11 +287,198 @@ transporter.sendMail(mailOptions, function(error, info){
 
 
 
+
+
+client2.on('message', function (topic, message) {
+  console.log("Topic name is receive from AUTH &&&  "+ topic + " Message content is  "+message);
+  var minThreshold , maxThreshold ;
+  
+Sensor.findById(topic, function (err, sensor) {
+      if (err){
+	  console.log("Error to get Sensor Attributes");
+      }else {
+		  
+var sensorName=sensor._id;
+
+  	actuatorsList.find({ 'sensor': sensorName }, function (err, actuators) {
+		  console.log("From Acturatos   ::: "+ actuators);
+if(err){
+		console.log(" Errors in actuators  is  : "+err);	
+}
+if(actuators != null){
+for(var i=0;i<actuators.length;i++){
+	console.log("Actuator Received is  : "+actuators[i].actName);
+	// Analysis Actions 
+	
+	var minThreshold=actuators[i].minthreshold;
+	var maxThreshold=actuators[i].maxthreshold;
+	var command=actuators[i].command;
+	var actuator=actuators[i].actName;
+					 console.log("Object returned is "+sensor);
+if(( parseInt(message) >= parseInt(maxThreshold) ) || (parseInt(message)  <= parseInt(minThreshold))){
+
+//console.log("send email"+parseInt(sensor.maxthreshold)  +"message "+ parseInt(message) );
+var eventSensor=sensor.eventSensor;
+
+if(actuator != "No" || actuator != "No	"){
+	console.log(" %%%%%%%%%%%% Message Publised to Event Trigger");
+	client2.publish(actuator, command);
+
+}
+		
+		
+		
+
+
+var mobile=sensor.mobile;
+		
+var email=sensor.email;
+if(sensor.sms == "sms"){
+
+
+
+console.log("****************** sending Tweets");
+	  User.findById(email, function (err, user) {
+
+
+		      
+			  
+
+		 
+		  
+		  
+		  
+var twit = new Twitter({
+  consumer_key: 'qu8pVznlr5URezLqEWByr96f8',
+  consumer_secret: 'KDKyqdcmgxxbgekSoQuRcms0HpdRzxyxxevrKaRhBqoTumqRP7',
+  access_token_key: user.accesstoken,
+  access_token_secret: user.acesstokensecret
+});
+ 
+
+
+twit.post('statuses/update', {status: '#IoT, Sensor Alert from SenseEgypt Platform Regarding your sensor measurements which is currently :  '+message +' at '+ Date.now()},  function(error, tweet, response) {
+  if(error) {
+	  
+	    console.log(error)
+  }
+  
+  console.log(tweet);  // Tweet body. 
+  console.log(response);  // Raw response object. 
+});
+
+
+ });
+}
+
+if(sensor.emailEvent == "email"){
+console.log("Sending Email");
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'senseegypt2020@gmail.com',
+        pass: '0105570894'
+    }
+});
+
+// NB! No need to recreate the transporter object. You can use
+// the same transporter object for all e-mails
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'SenseEgypt <senseegypt2020@gmail.com>', // sender address
+    to: email, // list of receivers
+    subject: 'SenseEgypt Sensor Alert', // Subject line
+    text: 'SenseEgypt Sensor Alert', // plaintext body
+    html: '<b>Sensor Alert from SenseEgypt Platform Regarding your sensor measurements which is currently :  '+message+'</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});
+
+}
+
+				 
+}
+
+
+	
+	
+	
+	
+	
+}
+
+	}else{
+		
+		        console.log('There is no Actuators: ');
+
+		
+		
+	}
+
+
+
+
+});
+
+
+
+	 
+}
+
+
+    });
+			
+			 
+			 
+  var id = mongoose.Types.ObjectId();
+  console.log(" Id Genrated is  "+id);
+
+  	        var sensorData = { _id: id };
+			sensorData.name=topic;
+	       sensorData.value=message;
+
+		   
+		           SensorDataMod.create(sensorData, function (err, newUser) {
+			console.log("Save sensor Data");
+          if (err) {
+            if (err instanceof mongoose.Error.ValidationError) {
+              return invalid();
+            } 
+         //   return next(err);
+          }
+		  
+
+
+  
+  
+        
+        })
+		
+		
+		
+		   
+
+});
+
+
+
+
+
 setInterval(publishTempMeasurements, 10*700);
 
 function publishTempMeasurements() {
 	
 	  client.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/TempratureSensor/VJ0M6UCNf14", ""+Math.floor(Math.random() * ((25-23)+1) + 23));
+	  client2.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/TempratureSensor/VJ0M6UCNf14", ""+Math.floor(Math.random() * ((25-23)+1) + 23));
 
 //	console.log("Message sent to broker "+Math.floor((Math.random() * 35) + 20) );
 
@@ -309,6 +499,7 @@ function publishMotionMeasurements() {
 		
 	}
 	  client.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/MotionDetector/N1muezj0M2", ""+motionM);
+	  client2.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/MotionDetector/N1muezj0M2", ""+motionM);
 
 	console.log(" ####### message sent to broker counter is  "+counter );
 counter=counter+3;
@@ -332,14 +523,16 @@ function publishMoitureMeasurements() {
         if (now.getHours() === 13 || now.getHours() === 14  ) {
 	 	moitureM=Math.floor(Math.random() * ((500-400)+1) + 400);
 
-	  client.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/Moisture/EyjjjnoAz3", ""+moitureM);
+				  client.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/Moisture/EyjjjnoAz3", ""+moitureM);
+				  client2.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/Moisture/EyjjjnoAz3", ""+moitureM);
 
         }else{
 				moitureM=Math.floor(Math.random() * ((45-30)+1) + 30);
 
 				  client.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/Moisture/EyjjjnoAz3", ""+moitureM);
 
-			
+							  client2.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/Moisture/EyjjjnoAz3", ""+moitureM);
+
 		}
 		
 		
@@ -357,7 +550,8 @@ function publishHumidityMeasurements() {
 				humidityM=Math.floor(Math.random() * ((37-33)+1) + 33);
 
 				  client.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/Humidity/V1t3CSkkQ2", ""+humidityM);
-		
+						  client2.publish("EgyptIOT/Portal/EGYIOT/sensor/STORMQ/Humidity/V1t3CSkkQ2", ""+humidityM);
+
 
 }
 
@@ -373,10 +567,12 @@ function publishWaterPumpMeasurements() {
         if (now.getHours() === 13 || now.getHours() === 14  ) {
 
 				  client.publish("EgyptIOT/Portal/EGYIOT/actuator/STORMQ/WaterPump/V1hMyUyy73", ""+1);
+				  client2.publish("EgyptIOT/Portal/EGYIOT/actuator/STORMQ/WaterPump/V1hMyUyy73", ""+1);
 
         }else{
 
 				  client.publish("EgyptIOT/Portal/EGYIOT/actuator/STORMQ/WaterPump/V1hMyUyy73", ""+0);
+				  client2.publish("EgyptIOT/Portal/EGYIOT/actuator/STORMQ/WaterPump/V1hMyUyy73", ""+0);
 
 			
 		}
@@ -402,6 +598,8 @@ function subscribeNewSensors() {
 						//			console.log("Registered to "+sensors[i]._id);
 
             client.subscribe(sensors[i]._id);
+			            client2.subscribe(sensors[i]._id);
+
 				}
 			}
 	}
